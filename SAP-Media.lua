8808644f4a527ee0042ea2358231a4a92f8f588f
mekert <mekert@nrtwebservices.com>
2025-03-26 18:57:11 -0500

    adding core files

---
+++ SAP-Media.lua
@@ -0,0 +1,6 @@
+local LSM = LibStub("LibSharedMedia-3.0")
+
+--Fonts
+LSM:Register("font","Expressway", [[Interface\Addons\NorthernSkyMedia\Media\Fonts\Expressway.TTF]])
+
+print("MyAddon successfully loaded!")
