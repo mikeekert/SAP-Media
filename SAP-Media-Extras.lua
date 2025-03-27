2452583d154184fe21ae190cf60824ac5168313b
mekert <mekert@nrtwebservices.com>
2025-03-26 23:43:51 -0500

    basic media viewer and my sample images for now

---
+++ SAP-Media-Extras.lua
@@ -0,0 +1,6 @@
+local LSM = LibStub("LibSharedMedia-3.0")
+--Fonts
+LSM:Register("font","Expressway", [[Interface\Addons\SAP-Media\Media\Fonts\Expressway.TTF]])
+
+--Sounds
+LSM:Register("sound", "|cFFFFD700Sonic|r", [[Interface\Addons\SAP-Media\Media\Sounds\sonic.ogg]])
