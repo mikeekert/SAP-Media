a82ae162ffc37cd897b7a004c0f73c8fd9d20a87
mekert <mekert@nrtwebservices.com>
2025-03-28 16:40:47 -0500

    imports added too

--- SAP-Media-Extras.lua
+++ SAP-Media-Extras.lua
@@ -4,3 +4,7 @@ LSM:Register("font","Expressway", [[Interface\Addons\SAP-Media\Media\Fonts\Expre
 
 --Sounds
 LSM:Register("sound", "|cFFFFD700Sonic|r", [[Interface\Addons\SAP-Media\Media\Sounds\sonic.ogg]])
+LSM:Register("sound", "|cFFFFD700Good|r", [[Interface\Addons\SAP-Media\Media\Sounds\good.ogg]])
+LSM:Register("sound", "|cFFFFD700Eww|r", [[Interface\Addons\SAP-Media\Media\Sounds\eww.ogg]])
+LSM:Register("sound", "|cFFFFD70Ah!|r", [[Interface\Addons\SAP-Media\Media\Sounds\lestatt.ogg]])
+LSM:Register("sound", "|cFFFFD700Crank|r", [[Interface\Addons\SAP-Media\Media\Sounds\staying_cranking.ogg]])
